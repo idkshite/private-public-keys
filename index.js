4bab5d574e581742d6f6b4cc9aefa67773099ccb
DATADEER <konto@datadeer.de>
2021-06-01 10:43:08 +0200

    correct missing import and redundant lines

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const crypto = require('crypto');
+const {PUBLIC_KEY_PAVEL, PRIVATE_KEY_PAVEL} = require("./keys.js");
 
 const encryptedMessage = encrypt(PUBLIC_KEY_PAVEL, "hello");
 const decryptedMessage = decrypt(PRIVATE_KEY_PAVEL, encryptedMessage);
 console.log(decryptedMessage.toString("utf8"));
-res.send(decryptedMessage.toString("utf8"));
 
 function encrypt(key, message) {
     const buffer = Buffer.from(message, 'utf8');
