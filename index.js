ba235ab9c84b4b9ebe80008b5b6af124ebb18517
idkshite <>
2021-06-01 22:14:04 +0000

    remove spaces

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const crypto = require('crypto');
 const {prettyLog, green, red} = require('./logger.js');
-
 const {PUBLIC_KEY_PAVEL, PRIVATE_KEY_PAVEL} = require("./keys.js");
+
 const topSecretPlaylist = "dancingqueen, whatsdoesthefoxsay, ymca";
 prettyLog("to be encrypted: ",topSecretPlaylist,green);
 
