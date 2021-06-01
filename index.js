b042d27562698810aa217ff0591e0029cf465805
idkshite <>
2021-06-01 22:13:17 +0000

    add spaces for better readability

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const {prettyLog, green, red} = require('./logger.js');
 
 const {PUBLIC_KEY_PAVEL, PRIVATE_KEY_PAVEL} = require("./keys.js");
 const topSecretPlaylist = "dancingqueen, whatsdoesthefoxsay, ymca";
-
 prettyLog("to be encrypted: ",topSecretPlaylist,green);
+
 const encryptedMessage = encrypt(PUBLIC_KEY_PAVEL,topSecretPlaylist);
 prettyLog("encrypted: ",encryptedMessage,red);
+
 const decryptedMessage = decrypt(PRIVATE_KEY_PAVEL, encryptedMessage);
 prettyLog("decrypted message: ",decryptedMessage,green);
 
