704cb0de06e3a86a6146357a7e3e0261e397db73
idkshite <>
2021-06-01 21:27:53 +0000

    add nice looking logs

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const crypto = require('crypto');
+
 const {PUBLIC_KEY_PAVEL, PRIVATE_KEY_PAVEL} = require("./keys.js");
 const topSecretPlaylist = "dancingqueen, whatsdoesthefoxsay, ymca";
+console.log("-------------")
 console.log("to be encrypted: ", topSecretPlaylist);
+console.log("-------------")
+console.log("\n");
+console.log("-------------")
 const encryptedMessage = encrypt(PUBLIC_KEY_PAVEL,topSecretPlaylist);
 console.log("encrypted: ",encryptedMessage);
+console.log("-------------")
+console.log("\n");
 const decryptedMessage = decrypt(PRIVATE_KEY_PAVEL, encryptedMessage);
-console.log(decryptedMessage.toString("utf8"));
+console.log("-------------")
+console.log("decrypted: ", decryptedMessage.toString("utf8"));
+console.log("-------------")
 
 function encrypt(key, message) {
     const buffer = Buffer.from(message, 'utf8');
