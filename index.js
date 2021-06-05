d3f44e8218466827f159e50721488a13de7204cb
DATADEER <konto@datadeer.de>
2021-06-05 18:11:18 +0200

    turn encycyption example into signing example

--- index.js
+++ index.js
@@ -3,23 +3,25 @@ const {prettyLog, green, red} = require('./logger.js');
 const {PUBLIC_KEY_PAVEL, PRIVATE_KEY_PAVEL} = require("./keys.js");
 
 const topSecretPlaylist = "dancingqueen, whatsdoesthefoxsay, ymca";
-prettyLog("to be encrypted: ",topSecretPlaylist,green);
+prettyLog("to be signed: ",topSecretPlaylist,green);
 
-const encryptedMessage = encrypt(PUBLIC_KEY_PAVEL,topSecretPlaylist);
-prettyLog("encrypted: ",encryptedMessage,red);
+const signature = sign(PRIVATE_KEY_PAVEL,topSecretPlaylist);
+prettyLog("signature: ",signature,red);
 
-const decryptedMessage = decrypt(PRIVATE_KEY_PAVEL, encryptedMessage);
-prettyLog("decrypted message: ",decryptedMessage,green);
+const decryptedMessage = verify(PUBLIC_KEY_PAVEL, signature, topSecretPlaylist);
+prettyLog("is same message: ",decryptedMessage,green);
 
-
-function encrypt(key, message) {
-    const buffer = Buffer.from(message, 'utf8');
-    const encrypted = crypto.publicEncrypt(key, buffer);
-    return encrypted.toString('base64');
+export function sign(key, message) {
+    return crypto.sign('sha256', Buffer.from(message), key).toString('base64');
 }
 
-function decrypt(key, cypher) {
-    const buffer = Buffer.from(cypher, 'base64');
-    const message = crypto.privateDecrypt(key, buffer).toString("utf8");
-    return message;
+export function verify(key, signature, toBeVerfiedData) {
+    const signature_buffer = Buffer.from(signature, 'base64');
+    const content_buffer = Buffer.from(toBeVerfiedData);
+    return crypto.verify('sha256', content_buffer, key, signature_buffer);
+}
+
+module.exports = {
+    sign,
+    verify
 }
