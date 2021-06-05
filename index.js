d0e19c5a68b747c9f252923c5afef0ea36274a91
DATADEER <konto@datadeer.de>
2021-06-05 18:14:45 +0200

    adjust key names and messages for better understanding

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const {prettyLog, green, red} = require('./logger.js');
 const {PUBLIC_KEY_PAVEL, PRIVATE_KEY_PAVEL} = require("./keys.js");
 
 const topSecretPlaylist = "dancingqueen, whatsdoesthefoxsay, ymca";
-prettyLog("to be encrypted: ",topSecretPlaylist,green);
+prettyLog("You encrypt with Mathildas public key: ",topSecretPlaylist,green);
 
-const encryptedMessage = encrypt(PUBLIC_KEY_PAVEL,topSecretPlaylist);
+const encryptedMessage = encrypt(PUBLIC_KEY_MATHILDA,topSecretPlaylist);
 prettyLog("encrypted: ",encryptedMessage,red);
 
-const decryptedMessage = decrypt(PRIVATE_KEY_PAVEL, encryptedMessage);
-prettyLog("decrypted message: ",decryptedMessage,green);
+const decryptedMessage = decrypt(PRIVATE_KEY_MATHILDA, encryptedMessage);
+prettyLog("Mathila decrypts with her private key: ",decryptedMessage,green);
 
 
 function encrypt(key, message) {
