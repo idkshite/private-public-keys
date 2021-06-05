b1f9bea174d5e8473fead240308b7b1891f057e8
DATADEER <konto@datadeer.de>
2021-06-05 18:17:05 +0200

    adjust key names and messages for better understanding * change key names on export

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const crypto = require('crypto');
 const {prettyLog, green, red} = require('./logger.js');
-const {PUBLIC_KEY_PAVEL, PRIVATE_KEY_PAVEL} = require("./keys.js");
+const {PRIVATE_KEY_YOU, PUBLIC_KEY_YOU} = require("./keys.js");
 
 const topSecretPlaylist = "dancingqueen, whatsdoesthefoxsay, ymca";
-prettyLog("to be signed: ",topSecretPlaylist,green);
+prettyLog("You sign with your private key: ",topSecretPlaylist,green);
 
-const signature = sign(PRIVATE_KEY_PAVEL,topSecretPlaylist);
+const signature = sign(PRIVATE_KEY_YOU,topSecretPlaylist);
 prettyLog("signature: ",signature,red);
 
-const decryptedMessage = verify(PUBLIC_KEY_PAVEL, signature, topSecretPlaylist);
-prettyLog("is same message: ",decryptedMessage,green);
+const decryptedMessage = verify(PUBLIC_KEY_YOU, signature, topSecretPlaylist);
+prettyLog("Mathilda verifies message with your public key: ",decryptedMessage,green);
 
 export function sign(key, message) {
     return crypto.sign('sha256', Buffer.from(message), key).toString('base64');
